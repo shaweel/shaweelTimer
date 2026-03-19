9edef0415a2bca4606abe0fda822009413141800
= <=>
2026-03-19 23:03:13 +0100

    upd prepupd

--- prepareUpdate.sh
+++ prepareUpdate.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+cd shaweelTimerGitHub
 git tag "$1"
 git push origin dev:stable --force
 git push origin "$1"
+cd ..
+cd shaweelTimerAur
+makepkg --printsrcinfo > ./.SRCINFO
+git add .
+git commit -m "$1"
+git push origin main
+cd ..
+cd shaweelTimerAurDev
+makepkg --printsrcinfo > ./.SRCINFO
+git add .
+git commit -m "$1"
+git push origin main
