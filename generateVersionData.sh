c5f1a62354aff63edae013c52e44f7fc55dab1dd
shaweel <shaweel@proton.me>
2026-03-19 13:30:09 +0100

    Remove flatpak

---
+++ generateVersionData.sh
@@ -0,0 +1,15 @@
+touch versionData.json
+
+fullBranch="$3"
+
+if [ "$fullBranch" = "dev" ]; then
+    	fullBranch="$3 (build: $4)"
+fi
+
+{
+    	echo "{"
+    	echo "\"type\": \"$1\","
+	echo "\"version\": \"$2\","
+	echo "\"branch\": \"$fullBranch\""
+    	echo "}"
+} > versionData.json
